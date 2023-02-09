dbe69e002e35c073a39ad47fe7c993e0bf967bba
unionerror <rajatdh27@gmail.com>
2023-02-09 22:08:39 +0530

    starting

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config();
+
+app.listen(8080, () => {
+  console.log("Backend running");
+});
