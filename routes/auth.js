02fff6b74154c7aa1774139832e8bf743360bd53
unionerror <rajatdh27@gmail.com>
2023-02-12 13:27:04 +0530

    login validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,40 @@
 const router = require("express").Router();
 const User = require("../model/User");
+const bcrypt = require("bcrypt");
 
 //register
 router.post("/register", async (req, res) => {
-  const newUser = await new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  });
   try {
+    //generate password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    //create new user
+    const newUser = await new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    //save new user
     const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//login
+router.post("/login", async (req, res) => {
+  try {
+    console.log("yes");
+    const user = await User.findOne({
+      email: req.body.email,
+    });
+    !user && res.status(404).json("user not found");
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(404).json("wrong password");
     res.status(200).json(user);
   } catch (err) {
     console.log(err);
