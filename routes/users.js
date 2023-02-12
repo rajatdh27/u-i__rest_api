f901cbe041204acbd1f0022bd2d66de7bf231c0f
unionerror <rajatdh27@gmail.com>
2023-02-12 13:55:10 +0530

    updating user

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,33 @@
+const User = require("../model/User");
+const bcrypt = require("bcrypt");
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.send("its user route");
+//update an user
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.user.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (err) {
+        return res.status(500).json(err);
+      }
+    }
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("Account has been updated");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can only update your account");
+  }
 });
+//delete user
+//get a user
+//follow user
+//unfollow user
 
 module.exports = router;
