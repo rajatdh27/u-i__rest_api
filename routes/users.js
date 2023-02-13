2e0e560918c82e4cc5d6464ecf3c3dbbe1be4a4a
unionerror <rajatdh27@gmail.com>
2023-02-13 21:45:11 +0530

    deleted user

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const router = require("express").Router();
 
 //update an user
 router.put("/:id", async (req, res) => {
-  if (req.body.userId === req.params.id || req.user.isAdmin) {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
     if (req.body.password) {
       try {
         const salt = await bcrypt.genSalt(10);
@@ -22,10 +22,24 @@ router.put("/:id", async (req, res) => {
       return res.status(500).json(err);
     }
   } else {
-    return res.status(403).json("You can only update your account");
+    return res.status(403).json("You can update only your account");
   }
 });
 //delete user
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("Account has been deleted");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can delete only your account");
+  }
+});
 //get a user
 //follow user
 //unfollow user
