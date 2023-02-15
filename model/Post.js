387cb44ef1c9e4f28b16781001e4846d1b95eaab
unionerror <rajatdh27@gmail.com>
2023-02-15 21:40:38 +0530

    post start

--- model/Post.js
+++ model/Post.js
@@ -21,4 +21,4 @@ const PostSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-module.exports = mongoose.model("User", PostSchema);
+module.exports = mongoose.model("Post", PostSchema);
--- routes/posts.js
+++ routes/posts.js
@@ -1,4 +1,20 @@
 const router = require("express").Router();
-router.get("/", req, (res) => {});
+const Post = require("../model/Post");
+
+//create a post
+router.get("/", async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//update a post
+//delete a post
+//like a post
+//get a post
+//get all posts
 
 module.exports = router;
