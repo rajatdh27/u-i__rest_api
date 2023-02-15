1fd3d52b283c38dbfd3ce9fe12d121176b80434e
unionerror <rajatdh27@gmail.com>
2023-02-15 21:48:29 +0530

    update a post

--- routes/posts.js
+++ routes/posts.js
@@ -12,6 +12,19 @@ router.get("/", async (req, res) => {
   }
 });
 //update a post
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("the post has been updated");
+    } else {
+      res.status(403).json("you can only your post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 //delete a post
 //like a post
 //get a post
